a7927854336e73d7836526d32c5d541cb1797ca2
Eliot <eliot@launchcode.org>
2016-12-07 16:07:03 -0600

    global js for google analytics;

---
+++ global_upload.js
@@ -0,0 +1,15 @@
+// START - Google Analytics Tracking Code
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ 
+$(document).ready(function(){
+  ga('create', 'UA-78748330-2', 'auto');
+  var sTemp = window.location.pathname.match(/\/courses\/(\d+)/);
+  var courseId = sTemp[1];
+  if (courseId) { // Only send the course id when we are on a course page
+    ga('set', 'dimension1', courseId);
+  }
+  ga('send', 'pageview');
+});
